c9f7fcbd572f134abd7c1d86cc387cc308c63efc
Dhanushya Dhanushya <84453418+DhanushyaG@users.noreply.github.com>
2023-01-05 19:39:51 +0530

    def hpy(n):     x=n     sum1=0     while x>0:         c=x%10         sum1=(sum1+(c*c))         x//=10     return sum1 n=int(input()) hpy(n) s=n while s>9:     s=hpy(s) if  s==1 or s==7:     print(True) else:     print(False)

--- happy_number.py
+++ happy_number.py
@@ -1,13 +1,17 @@
+def hpy(n):
+    x=n
+    sum1=0
+    while x>0:
+        c=x%10
+        sum1=(sum1+(c*c))
+        x//=10
+    return sum1
 n=int(input())
-s=0
-while n>0:
-    r=n%10
-    n=n//10
-    s+=r**2
-    if n==0 and s>9:
-        n=s
-        s=0
-if s==1 or s==7:
-    print('True')
+hpy(n)
+s=n
+while s>9:
+    s=hpy(s)
+if  s==1 or s==7:
+    print(True)
 else:
-    print('False')
+    print(False)
